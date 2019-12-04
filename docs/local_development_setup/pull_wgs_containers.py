a3370b783e26d8b7489642035d83dc101a40c573
Ubuntu <abramsd@abramsdUbuntuBase.ywc4vxmbmevelk0odry01j5ind.ux.internal.cloudapp.net>
2019-12-04 20:10:27 +0000

    made pull script user-friendly

--- docs/local_development_setup/pull_wgs_containers.py
+++ docs/local_development_setup/pull_wgs_containers.py
@@ -1,12 +1,36 @@
 import yaml
 import sys
 import subprocess
+import argparse
 
-containers = yaml.load(open(sys.argv[1]), Loader=yaml.FullLoader)['wgs_containers']
-cmd = "docker pull {}"
-for name, container in containers.items():
-    runcmd = cmd.format(container)
-    process = subprocess.Popen(runcmd.split(), stdout = subprocess.PIPE)
-    output, error = process.communicate()
-    print ("pulling container {}".format(name), 
-        "\noutput: ", output)
+def pull_containers(containers):
+    '''
+    pulls all docker containers from yaml
+    file config.
+
+    :param containers: yaml with docker containers
+    '''
+    cmd = "docker pull {}"
+    for name, container in containers.items():
+        runcmd = cmd.format(container)
+        process = subprocess.Popen(runcmd.split(), stdout = subprocess.PIPE)
+        output, error = process.communicate()
+        print ("pulling container {}".format(name), 
+            "\noutput: ", output)
+
+def main(): 
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('containers_yaml', 
+        help='yaml containing the wgs docker containers. See containers.yaml')
+    
+    args = parser.parse_args()
+    
+    containers_yaml = vars(args)["containers_yaml"]
+    containers = yaml.load(open(containers_yaml), Loader=yaml.FullLoader)['wgs_containers']
+    
+    pull_containers(containers)
+
+if __name__ == "__main__":
+    main()
+
