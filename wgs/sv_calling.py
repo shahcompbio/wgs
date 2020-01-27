47815a6bc134b590735c901934a0e51d561fe348
Diljot Grewal <diljot.grewal@gmail.com>
2020-01-27 16:01:08 -0500

    destruct: sample id in filenames

--- wgs/sv_calling.py
+++ wgs/sv_calling.py
@@ -23,13 +23,13 @@ def sv_calling_workflow(args):
     samples = tumours.keys()
 
     sv_outdir = os.path.join(args['out_dir'], 'breakpoints', '{sample_id}')
-    destruct_breakpoints = os.path.join(sv_outdir, 'destruct_breakpoints.csv.gz')
-    destruct_library = os.path.join(sv_outdir, 'destruct_library.csv.gz')
-    destruct_raw_breakpoints = os.path.join(sv_outdir, 'destruct_raw_breakpoints.csv.gz')
-    destruct_raw_library = os.path.join(sv_outdir, 'destruct_raw_library.csv.gz')
-    destruct_reads = os.path.join(sv_outdir, 'destruct_reads.csv.gz')
-    lumpy_vcf = os.path.join(sv_outdir, 'lumpy.vcf')
-    parsed_csv = os.path.join(sv_outdir, 'filtered_consensus_calls.csv.gz')
+    destruct_breakpoints = os.path.join(sv_outdir, '{sample_id}_destruct_breakpoints.csv.gz')
+    destruct_library = os.path.join(sv_outdir, '{sample_id}_destruct_library.csv.gz')
+    destruct_raw_breakpoints = os.path.join(sv_outdir, '{sample_id}_destruct_raw_breakpoints.csv.gz')
+    destruct_raw_library = os.path.join(sv_outdir, '{sample_id}_destruct_raw_library.csv.gz')
+    destruct_reads = os.path.join(sv_outdir, '{sample_id}_destruct_reads.csv.gz')
+    lumpy_vcf = os.path.join(sv_outdir, '{sample_id}_lumpy.vcf')
+    parsed_csv = os.path.join(sv_outdir, '{sample_id}_filtered_consensus_calls.csv.gz')
 
     single_node = args['single_node']
 
