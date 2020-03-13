87e7cd8d39cd8fae3648a5f2cf0c56d5afa3f0ce
Diljot Grewal <diljot.grewal@gmail.com>
2020-03-13 13:53:07 -0400

    realignment:: testing

--- wgs/workflows/realignment/tasks.py
+++ wgs/workflows/realignment/tasks.py
@@ -7,25 +7,26 @@ Created on Aug, 2019
 import os
 
 import pypeliner
-
-from wgs.utils import helpers
-
 import pysam
 import time
-
-def split_bam_by_readgroups(infile, readgroups, r1_fastq_output, r2_fastq_output):
-    for readgroup in readgroups:
-        cmd = ['wgs_bamtofastq', infile, r1_fastq_output[readgroup], r2_fastq_output[readgroup], '--readgroup',
-               readgroup]
-        pypeliner.commandline.execute(*cmd)
+from wgs.utils import helpers
 
 
 def split_by_rg(infile, read1_output, read2_output, tempdir):
-
     helpers.makedirs(tempdir)
+
+    print("***********")
+    print(tempdir)
+    print(os.listdir(tempdir))
+    print("***********")
 
     cmd = ['wgs_bamtofastq', infile, tempdir]
     pypeliner.commandline.execute(*cmd)
+
+    print("***********")
+    print(tempdir)
+    print(os.listdir(tempdir))
+    print("***********")
 
     try:
         readgroups = os.listdir(tempdir)
@@ -34,7 +35,6 @@ def split_by_rg(infile, read1_output, read2_output, tempdir):
         readgroups = os.listdir(tempdir)
 
     for readgroup in readgroups:
-
         os.rename(
             os.path.join(tempdir, readgroup, 'R1.fastq.gz'),
             read1_output[readgroup]
