12c22c752706b871a1718661bd553147ad32388c
Diljot Grewal <diljot.grewal@gmail.com>
2020-03-27 17:32:19 -0400

    fix: consensus calling with types

--- wgs/workflows/breakpoint_calling_consensus/scripts/consensus.py
+++ wgs/workflows/breakpoint_calling_consensus/scripts/consensus.py
@@ -11,20 +11,44 @@ def build_interval_tree(data):
     itree = defaultdict(IntervalTree)
 
     for val in data:
-        chrom, pos, ci, brk_id = val
+        chrom, pos, ci, brk_type = val
 
         ci = ci.split(',')
 
         start = pos + float(ci[0])
         end = pos + float(ci[1]) + 1
 
-        itree[chrom].addi(start, end, brk_id)
+        itree[chrom].addi(start, end, brk_type)
 
     return itree
 
 
-def check_olp(interval_tree, chrom, pos):
-    if interval_tree[chrom][pos]:
+def compare_lumpy_destruct_type(lumpy_type, destruct_type):
+    if lumpy_type not in ['DEL', 'INV', 'DUP', 'BND']:
+        raise Exception('Unknown lumpy type: {}'.format(lumpy_type))
+
+    if lumpy_type not in ['deletion', 'inversion', 'duplication', 'translocation']:
+        raise Exception('Unknown lumpy type: {}'.format(lumpy_type))
+
+    if lumpy_type == 'DEL' and destruct_type == 'deletion':
+        return True
+    elif lumpy_type == 'INV' and destruct_type == 'inversion':
+        return True
+    elif lumpy_type == 'DUP' and destruct_type == 'duplication':
+        return True
+    elif lumpy_type == 'BND' and destruct_type == 'translocation':
+        return True
+    else:
+        return False
+
+
+def check_olp(interval_tree, chrom, pos, destruct_type):
+    overlap = interval_tree[chrom][pos]
+
+    lumpy_types = [v[2] for v in overlap]
+    lumpy_types = [compare_lumpy_destruct_type(v, destruct_type) for v in lumpy_types]
+
+    if lumpy_types and any(lumpy_types):
         return True
     else:
         return False
@@ -36,13 +60,16 @@ def load_data(infile):
 
 def load_lumpy_into_tree(lumpy_df, confidence_interval=None):
     # Add lumpy breakpoint id to each zipped entry
+
     if confidence_interval:
         confidence_interval = '-{},{}'.format(confidence_interval, confidence_interval)
-        data = list(zip(lumpy_df.chromosome_1, lumpy_df.position_1, [confidence_interval] * len(lumpy_df)))
-        data += list(zip(lumpy_df.chromosome_2, lumpy_df.position_2, [confidence_interval] * len(lumpy_df)))
+        data = list(
+            zip(lumpy_df.chromosome_1, lumpy_df.position_1, [confidence_interval] * len(lumpy_df), lumpy_df.SVTYPE))
+        data += list(
+            zip(lumpy_df.chromosome_2, lumpy_df.position_2, [confidence_interval] * len(lumpy_df), lumpy_df.SVTYPE))
     else:
-        data = list(zip(lumpy_df.chromosome_1, lumpy_df.position_1, lumpy_df['CIPOS']))
-        data += list(zip(lumpy_df.chromosome_2, lumpy_df.position_2, lumpy_df['CIEND']))
+        data = list(zip(lumpy_df.chromosome_1, lumpy_df.position_1, lumpy_df['CIPOS'], lumpy_df['SVTYPE']))
+        data += list(zip(lumpy_df.chromosome_2, lumpy_df.position_2, lumpy_df['CIEND'], lumpy_df['SVTYPE']))
 
     intervaltree = build_interval_tree(data)
 
@@ -50,20 +77,12 @@ def load_lumpy_into_tree(lumpy_df, confidence_interval=None):
 
 
 def filter_destruct_on_lumpy(destruct, lumpy_tree):
-    destruct['is_lumpy_filtered'] = False
-
-    for idx in destruct.index:
-        lumpy_brk_ids = dict()
-
-        for break_end in ('1', '2'):
-            chrom = destruct.loc[idx, 'chromosome_{break_end}'.format(break_end)]
-            pos = destruct.loc[idx, 'position_{break_end}'.format(break_end)]
-
-            for ival in lumpy_tree[chrom][pos]:
-                lumpy_brk_ids[break_end] = set([ival[2] for ival in lumpy_tree[chrom][pos]])
-
-        if len(lumpy_brk_ids['1'].intersection(lumpy_brk_ids['2'])) > 0:
-            destruct.loc[idx, 'is_lumpy_filtered'] = True
+    destruct = destruct[
+        destruct.apply(lambda x: check_olp(lumpy_tree, x['chromosome_1'], x['position_1'], x['rearrangement_type']),
+                       axis=1)]
+    destruct = destruct[
+        destruct.apply(lambda x: check_olp(lumpy_tree, x['chromosome_2'], x['position_2'], x['rearrangement_type']),
+                       axis=1)]
 
     return destruct
 
