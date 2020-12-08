dd6278a545951d2111c42d455f28c435fac4ffa1
DouglasAbrams <32776088+DouglasAbrams@users.noreply.github.com>
2020-12-08 17:48:28 -0500

    cna annotation +germline/somtic labeling (#19)
    * verson compatability tested (#18)
    
    * verson compatability tested
    
    * wgs qc docs
    
    Co-authored-by: Douglas Abrams <abramsd@ceto.cbio.private>
    
    * cna annotation +germline/somtic labeling
    
    * reduced memory load of burden plot, somatic only now
    
    * new output: remixt segmental copynumber for cohort
    
    Co-authored-by: Douglas Abrams <abramsd@ceto.cbio.private>

--- wgs/workflows/cohort_qc/tasks.py
+++ wgs/workflows/cohort_qc/tasks.py
@@ -2,22 +2,87 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pypeliner
 from wgs.utils import helpers
+from classifycopynumber import parsers, transformations
 
 
-def merge_mafs(mafs, merged_maf, labels):
+def merge_segmental_cn(segmental_cn, concats):
+    files = [pd.read_csv(f, sep="\t") for f in list(segmental_cn.values())]
+    segmental_cn_combined = pd.concat(files)
+    segmental_cn_combined.to_csv(concats, sep="\t", index=False)
+
+
+def generate_segmental_copynumber(remixt, segmental_cn, sample):
+    cn, stats = parsers.read_remixt(remixt)
+    cn["sample"] = [sample] * len(cn)
+
+    transformations.generate_segmental_cn(segmental_cn, cn, stats)
+
+
+def merge_cna_tables(amps, dels, labels, output, cohort):
+    cna = {d1: [a,d] for (d1, a), (d2, d) in zip(amps.items(), dels.items()) }
+    number=0
+    for s, files in cna.items():
+        
+        label = labels[(cohort, s)]
+        for f in files:
+        
+            data = pd.read_csv(f, usecols=["cn_type", "gene_name", "pass_filter"])
+
+            data=data[data.pass_filter == True]
+
+            data=data.rename(columns={"cn_type": "CN", "gene_name":"Gene"})
+
+            data["Sample_name"] = [label] * len(data)
+
+            data = data[["Gene", "Sample_name", "CN"]]
+
+            if number==0:
+                header=True
+            else:
+                header=False
+
+            if not data.empty:
+                number+=1
+                data.to_csv(output, index=False, mode='a', header=header, sep="\t")
+
+
+def classify_remixt(sample_label, remixt, gtf, output_dir, amps, dels, docker_image=None):
+
+    cmd = [
+        "classifycopynumber", gtf, output_dir, sample_label, amps, dels, "--remixt_h5_filename", remixt, "--plot", False
+    ]
+
+    pypeliner.commandline.execute(*cmd, docker_image=docker_image)
+
+
+
+def merge_mafs(mafs, merged_maf, write_header=True):
+    for m in mafs:
+
+        maf = pd.read_csv(m, sep="\t", chunksize=10e6)
+        for chunk in maf:
+
+            chunk.to_csv(merged_maf, sep="\t", index=False, header=write_header, mode='a')
+        #only write the first header
+        write_header=False   
+
+
+def merge_relabel_mafs(mafs, merged_maf, labels, class_label=None, write_header=True):
 
     mafs = list(mafs.values())
     labels = list(labels.values())
 
     #only write the first header
-    write_header = True
-
+    #TODO better v names
     for label, maf in zip(labels, mafs):
-        maf = pd.read_csv(maf, sep="\t", chunksize=10e6, skiprows=1)
-        for chunk in maf:
-            if labels:
-                chunk["Tumor_Sample_Barcode"] = [label] * len(chunk.index)
-            chunk.to_csv(merged_maf, sep="\t", index=False, header=write_header, mode='a')
+        if maf:
+            maf = pd.read_csv(maf, sep="\t", chunksize=10e6, skiprows=1)
+            for chunk in maf:
+                if labels:
+                    chunk["Tumor_Sample_Barcode"] = [label] * len(chunk.index)
+                if class_label:
+                    chunk["Variant_Classification"] = chunk.Variant_Classification.apply(lambda c: c+class_label)
+                chunk.to_csv(merged_maf, sep="\t", index=False, header=write_header, mode='a')
 
             #only write the first header
             write_header=False
@@ -42,12 +107,11 @@ def filter_annotated_maf(annotated_maf, filtered_maf):
         (annotated_maf.ONCOGENIC == "Oncogenic")
         | (annotated_maf.ONCOGENIC == "Likely Oncogenic")
         ]
-
     filt_maf.to_csv(filtered_maf, sep="\t")
 
 
 def plot_mutation_burden(maf, burden_plot_path):
-    maf = pd.read_csv(maf, sep="\t").drop_duplicates()
+    maf = pd.read_csv(maf, sep="\t", usecols=["Tumor_Sample_Barcode"]).drop_duplicates()
     data = maf.groupby("Tumor_Sample_Barcode").size().sort_values(ascending=False)
 
     fig, axis = plt.subplots(figsize=(15, 5))
@@ -60,23 +124,26 @@ def plot_mutation_burden(maf, burden_plot_path):
 
 
 def make_R_cohort_plots(
-        cohort_maf, oncoplot_path, somatic_interactions,
-        mafsummary, filtered_maf=None
+        cohort_maf, cntable, oncoplot_path, somatic_interactions,
+        mafsummary, filtered_maf=None, docker_image=None
 ):
     if not filtered_maf:
         filtered_maf = cohort_maf
 
     plots_cmd = [
-        "maftools_plots.R", cohort_maf, filtered_maf,
+        "maftools_plots.R", cohort_maf, cntable, filtered_maf,
         oncoplot_path, somatic_interactions, mafsummary
     ]
 
-    pypeliner.commandline.execute(*plots_cmd)
+    pypeliner.commandline.execute(*plots_cmd, docker_image=docker_image)
 
 
-def make_report(cohort_label, oncoplot, somatic_interactions, mafsummary, burden_plot, report_path):
+def make_report(cohort_label, oncoplot, somatic_interactions, mafsummary, 
+    burden_plot, report_path, docker_image=None
+):
     cmd = [
-        "run_report.sh", report_path, cohort_label, oncoplot,
+        "run_cohort_qc_report.sh", report_path, cohort_label, oncoplot,
         somatic_interactions, mafsummary, burden_plot
     ]
-    pypeliner.commandline.execute(*cmd)
+    pypeliner.commandline.execute(*cmd, docker_image=docker_image)
+
