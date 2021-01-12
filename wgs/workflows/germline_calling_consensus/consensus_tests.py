a66b7c53577e652a3d02f0cd8eb6f507bbbf8d15
Diljot Grewal <diljot.grewal@gmail.com>
2021-01-12 12:22:17 -0800

    test outline for douglas

---
+++ wgs/workflows/germline_calling_consensus/consensus_tests.py
@@ -0,0 +1,126 @@
+
+
+
+
+def museq_strelka_snv_consensus():
+    """
+    add some calls to museq and strelka
+
+    check if they're in consensus
+
+
+    Returns
+    -------
+
+    """
+    pass
+
+
+def mutect_strelka_snv_consensus():
+    """
+    add some calls to mutect and strelka,
+
+    check if they're in consensus
+
+
+    Returns
+    -------
+
+    """
+    pass
+
+
+def museq_mutect_snv_consensus():
+    """
+    add some calls to museq and mutect
+
+    check if they're in consensus
+
+
+    Returns
+    -------
+
+    """
+    pass
+
+
+def museq_mutect_strelka_snv_consensus():
+    """
+    add some calls to museq and strelka
+
+    check if they're in consensus
+
+
+    Returns
+    -------
+
+    """
+    pass
+
+
+
+def mutect_indel_consensus():
+    """
+    add some calls to museq and strelka
+
+    check if they're in consensus
+
+
+    Returns
+    -------
+
+    """
+    pass
+
+def strelka_indel_consensus():
+    """
+    add some calls to museq and strelka
+
+    check if they're in consensus
+
+
+    Returns
+    -------
+
+    """
+    pass
+
+def mutect_strelka_indel_consensus():
+    """
+    add some calls to museq and strelka
+
+    check if they're in consensus
+
+
+    Returns
+    -------
+
+    """
+    pass
+
+
+
+"""
+one unit test per case
+
+
+case 1: A-> AT and AT->ATT
+case 2: GTA -> G and GTATA -> GTA
+case 3: CA -> CTA  and C -> CT 
+case 4: T -> TAC and T -> TACACAC
+
+
+1. just check the normalize function for each case
+2. generate inputs with these cases for indel callers and see if the code works on the whole
+
+
+"""
+
+
+
+"""
+
+check counts extraction
+
+write test cases per data type. and verify counts output
+"""
