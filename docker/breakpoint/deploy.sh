954fee6e07047b01f2739b24693ab96b40ee1417
Diljot Grewal <diljot.grewal@gmail.com>
2021-03-23 08:57:43 -0700

    codebuild: fixed alignment docker, updated posstbuild to skip rebuilds

---
+++ docker/breakpoint/deploy.sh
@@ -0,0 +1,12 @@
+REGISTRY=$1
+ORG=$2
+
+TESTORG=$3
+
+echo "\n LOGIN \n"
+docker login $REGISTRY -u $4 --password $5
+
+docker pull $REGISTRY/$TESTORG/wgs_breakpoint:$TAG
+docker tag $REGISTRY/$TESTORG/wgs_breakpoint:$TAG $REGISTRY/$ORG/wgs_breakpoint:$TAG
+docker push $REGISTRY/$ORG/wgs_breakpoint:$TAG
+
