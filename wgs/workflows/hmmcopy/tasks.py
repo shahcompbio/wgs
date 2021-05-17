b60407d346a83139d47fba5ef9097c38db22706c
Diljot Grewal <diljot.grewal@gmail.com>
2021-05-17 07:32:49 -0700

    bug: added single sample hmmcopy to arhs

--- wgs/workflows/hmmcopy/tasks.py
+++ wgs/workflows/hmmcopy/tasks.py
@@ -105,10 +105,10 @@ def plot_hmm(
     pdfutils.merge_jpgs(correction_pdfs, correction_pdf)
 
     all_hmmcopy_pdfs = [os.path.join(hmmcopy_plots_dir, pdf)
-                            for pdf in os.listdir(hmmcopy_plots_dir)]
+                        for pdf in os.listdir(hmmcopy_plots_dir)]
     # just some sorting
     human_pdfs = [os.path.join(hmmcopy_plots_dir, 'chr_{}.pdf'.format(chrom))
-                      for chrom in list(map(str, range(1,23))) + ['X']]
+                  for chrom in list(map(str, range(1, 23))) + ['X']]
     all_hmmcopy_pdfs = [v for v in human_pdfs if v in all_hmmcopy_pdfs]
     all_hmmcopy_pdfs += list(set(all_hmmcopy_pdfs) - set(human_pdfs))
 
