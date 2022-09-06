aced15d3b7e28c97ebb74079f2c3959488667c7d
Diljot Grewal <diljot.grewal@gmail.com>
2022-09-06 11:14:42 -0700

    v0.2.9

--- wgs/breakpoint_calling.py
+++ wgs/breakpoint_calling.py
@@ -19,21 +19,21 @@ def breakpoint_calling_workflow(args):
     meta_yaml = os.path.join(args["out_dir"], 'metadata.yaml')
     input_yaml_blob = os.path.join(args["out_dir"], 'input.yaml')
 
-    tumours = helpers.get_values_from_input(inputs, 'tumour')
-    normals = helpers.get_values_from_input(inputs, 'normal')
-    samples = list(tumours.keys())
+    tumour = inputs['tumour']
+    normal = inputs['normal']
+    sample_id = inputs['sample_id']
 
-    sv_outdir = os.path.join(args['out_dir'], 'breakpoints', '{sample_id}')
-    destruct_breakpoints = os.path.join(sv_outdir, '{sample_id}_destruct_breakpoints.csv.gz')
-    destruct_library = os.path.join(sv_outdir, '{sample_id}_destruct_library.csv.gz')
-    destruct_raw_breakpoints = os.path.join(sv_outdir, '{sample_id}_destruct_raw_breakpoints.csv.gz')
-    destruct_raw_library = os.path.join(sv_outdir, '{sample_id}_destruct_raw_library.csv.gz')
-    destruct_reads = os.path.join(sv_outdir, '{sample_id}_destruct_reads.csv.gz')
-    lumpy_vcf = os.path.join(sv_outdir, '{sample_id}_lumpy.vcf')
-    parsed_csv = os.path.join(sv_outdir, '{sample_id}_filtered_consensus_calls.csv.gz')
+    destruct_breakpoints = args['output_prefix'] + '_destruct_breakpoints.csv.gz'
+    destruct_library = args['output_prefix'] + '_destruct_library.csv.gz'
+    destruct_raw_breakpoints = args['output_prefix'] + '_destruct_raw_breakpoints.csv.gz'
+    destruct_raw_library = args['output_prefix'] + '_destruct_raw_library.csv.gz'
+    destruct_reads = args['output_prefix'] + '_destruct_reads.csv.gz'
 
-    svaba_vcf = os.path.join(sv_outdir, '{sample_id}_svaba.vcf')
+    lumpy_vcf = args['output_prefix'] + '_lumpy.vcf'
 
+    svaba_vcf = args['output_prefix'] + '_svaba.vcf'
+
+    parsed_csv = args['output_prefix'] + '_filtered_consensus_calls.csv.gz'
 
     single_node = args['single_node']
 
@@ -42,25 +42,18 @@ def breakpoint_calling_workflow(args):
 
     workflow = pypeliner.workflow.Workflow()
 
-    workflow.setobj(
-        obj=mgd.OutputChunks('sample_id'),
-        value=samples)
-
     workflow.subworkflow(
         name='destruct',
         func=destruct_wgs.create_destruct_wgs_workflow,
-        axes=('sample_id',),
         args=(
-            mgd.InputFile("tumour.bam", 'sample_id', fnames=tumours,
-                          extensions=['.bai'], axes_origin=[]),
-            mgd.InputFile("normal.bam", 'sample_id', fnames=normals,
-                          extensions=['.bai'], axes_origin=[]),
-            mgd.OutputFile('destruct_raw_breakpoints', 'sample_id', template=destruct_raw_breakpoints),
-            mgd.OutputFile('destruct_raw_library', 'sample_id', template=destruct_raw_library),
-            mgd.OutputFile('destruct_breakpoints', 'sample_id', template=destruct_breakpoints),
-            mgd.OutputFile('destruct_library', 'sample_id', template=destruct_library),
-            mgd.OutputFile('destruct_reads', 'sample_id', template=destruct_reads),
-            mgd.InputInstance('sample_id'),
+            mgd.InputFile(tumour, extensions=['.bai']),
+            mgd.InputFile(normal, extensions=['.bai']),
+            mgd.OutputFile(destruct_raw_breakpoints),
+            mgd.OutputFile(destruct_raw_library),
+            mgd.OutputFile(destruct_breakpoints),
+            mgd.OutputFile(destruct_library),
+            mgd.OutputFile(destruct_reads),
+            sample_id,
             refdir_paths['reference'],
             refdir_paths['refdata_destruct'],
             refdir_paths['gtf']
@@ -71,31 +64,24 @@ def breakpoint_calling_workflow(args):
     workflow.subworkflow(
         name='lumpy',
         func=lumpy.create_lumpy_workflow,
-        axes=('sample_id',),
         args=(
-            mgd.OutputFile('lumpy_vcf', 'sample_id', template=lumpy_vcf),
+            mgd.OutputFile(lumpy_vcf),
         ),
         kwargs={
-            'tumour_bam': mgd.InputFile(
-                "tumour.bam", 'sample_id', fnames=tumours,
-                extensions=['.bai'], axes_origin=[]),
-            'normal_bam': mgd.InputFile(
-                "normal.bam", 'sample_id', fnames=normals,
-                extensions=['.bai'], axes_origin=[]),
+            'tumour_bam': mgd.InputFile(tumour, extensions=['.bai']),
+            'normal_bam': mgd.InputFile(normal, extensions=['.bai']),
             'single_node': single_node
         },
     )
-
 
     if args['svaba']:
         workflow.subworkflow(
             name='svaba',
             func=svaba.create_svaba_workflow,
-            axes=('sample_id',),
             args=(
-                mgd.InputFile("tumour.bam", 'sample_id', fnames=tumours, extensions=['.bai'], axes_origin=[]),
-                mgd.InputFile("normal.bam", 'sample_id', fnames=normals, extensions=['.bai'], axes_origin=[]),
-                mgd.OutputFile('svaba_vcf', 'sample_id', template=svaba_vcf),
+                mgd.InputFile(tumour, extensions=['.bai']),
+                mgd.InputFile(normal, extensions=['.bai']),
+                mgd.OutputFile(svaba_vcf),
                 refdir_paths['reference'],
             ),
         )
@@ -105,9 +91,9 @@ def breakpoint_calling_workflow(args):
         func=breakpoint_calling_consensus.create_consensus_workflow,
         axes=('sample_id',),
         args=(
-            mgd.InputFile('destruct_breakpoints', 'sample_id', template=destruct_breakpoints),
-            mgd.InputFile('lumpy_vcf', 'sample_id', template=lumpy_vcf),
-            mgd.OutputFile('consensus_calls', 'sample_id', template=parsed_csv, extensions=['.yaml']),
+            mgd.InputFile(destruct_breakpoints),
+            mgd.InputFile(lumpy_vcf),
+            mgd.OutputFile(parsed_csv, extensions=['.yaml']),
             chromosomes
         ),
     )
@@ -125,15 +111,13 @@ def breakpoint_calling_workflow(args):
     if args['svaba']:
         filenames.append(svaba_vcf)
 
-    outputted_filenames = helpers.expand_list(filenames, samples, "sample_id")
-
     workflow.transform(
         name='generate_meta_files_results',
         func=helpers.generate_and_upload_metadata,
         args=(
             sys.argv[0:],
             args["out_dir"],
-            outputted_filenames,
+            filenames,
             mgd.OutputFile(meta_yaml)
         ),
         kwargs={
