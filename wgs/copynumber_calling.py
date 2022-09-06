816eb3e1e87bcbd5806438d71a632a561216ba6e
Diljot Grewal <diljot.grewal@gmail.com>
2022-09-06 12:20:49 -0700

    v0.2.9

--- wgs/copynumber_calling.py
+++ wgs/copynumber_calling.py
@@ -27,66 +27,54 @@ def copynumber_calling_workflow(args):
     meta_yaml = os.path.join(outdir, 'metadata.yaml')
     input_yaml_blob = os.path.join(outdir, 'input.yaml')
 
-    tumours = helpers.get_values_from_input(inputs, 'tumour')
-    normals = helpers.get_values_from_input(inputs, 'normal')
-    targets = helpers.get_values_from_input(inputs, 'target_list')
-    breakpoints = helpers.get_values_from_input(inputs, 'breakpoints')
-    samples = list(tumours.keys())
+    normal = inputs['normal']
+    tumour = inputs['tumour']
+    targets = inputs['targets']
+    breakpoints = inputs['breakpoints']
+    sample_id = inputs['sample_id']
 
-    cna_outdir = os.path.join(args['out_dir'], 'copynumber', '{sample_id}')
+    titan_outfile = args['output_prefix'] + '_titan_markers.csv.gz'
+    titan_params = args['output_prefix'] + '_titan_params.csv.gz'
+    titan_segs = args['output_prefix'] + '_titan_segs.csv.gz'
+    titan_igv_segs = args['output_prefix'] + '_titan_igv_segs.seg'
+    titan_parsed = args['output_prefix'] + '_titan_parsed.csv.gz'
+    titan_plots = args['output_prefix'] + '_titan_plots.pdf'
+    titan_tar_outputs = args['output_prefix'] + '_data_all_parameters.tar.gz'
+    museq_vcf = args['output_prefix'] + '_museq.vcf'
 
-    titan_raw_dir = os.path.join(cna_outdir, 'titan')
+    remixt_outfile = args['output_prefix'] + '_remixt.h5'
+    remixt_raw_dir = args['output_prefix'] + '_raw_dir'
 
-    titan_outfile = os.path.join(titan_raw_dir, '{sample_id}_titan_markers.csv.gz')
-    titan_params = os.path.join(titan_raw_dir, '{sample_id}_titan_params.csv.gz')
-    titan_segs = os.path.join(titan_raw_dir, '{sample_id}_titan_segs.csv.gz')
-    titan_igv_segs = os.path.join(titan_raw_dir, '{sample_id}_titan_igv_segs.seg')
-    titan_parsed = os.path.join(titan_raw_dir, '{sample_id}_titan_parsed.csv.gz')
-    titan_plots = os.path.join(titan_raw_dir, '{sample_id}_titan_plots.pdf')
-    titan_tar_outputs = os.path.join(titan_raw_dir, '{sample_id}_data_all_parameters.tar.gz')
-    museq_vcf = os.path.join(titan_raw_dir, '{sample_id}_museq.vcf')
-
-    remixt_outdir = os.path.join(args['out_dir'], 'remixt', '{sample_id}')
-    remixt_outfile = os.path.join(remixt_outdir, '{sample_id}_remixt.h5')
-    remixt_raw_dir = os.path.join(remixt_outdir, '{sample_id}_raw_dir')
-
-    remixt_brk_cn_csv = os.path.join(remixt_outdir, '{sample_id}_remixt_brk_cn.csv.gz')
-    remixt_cn_csv = os.path.join(remixt_outdir, '{sample_id}_remixt_cn.csv.gz')
-    remixt_minor_modes_csv = os.path.join(remixt_outdir, '{sample_id}_remixt_minor_modes.csv.gz')
-    remixt_mix_csv = os.path.join(remixt_outdir, '{sample_id}_remixt_mix.csv.gz')
-    remixt_read_depth_csv = os.path.join(remixt_outdir, '{sample_id}_remixt_read_depth.csv.gz')
-    remixt_stats_csv = os.path.join(remixt_outdir, '{sample_id}_remixt_stats.csv.gz')
+    remixt_brk_cn_csv = args['output_prefix'] + '_remixt_brk_cn.csv.gz'
+    remixt_cn_csv = args['output_prefix'] + '_remixt_cn.csv.gz'
+    remixt_minor_modes_csv = args['output_prefix'] + '_remixt_minor_modes.csv.gz'
+    remixt_mix_csv = args['output_prefix'] + '_remixt_mix.csv.gz'
+    remixt_read_depth_csv = args['output_prefix'] + '_remixt_read_depth.csv.gz'
+    remixt_stats_csv = args['output_prefix'] + '_remixt_stats.csv.gz'
 
     refdir_paths = config.refdir_data(args['refdir'])['paths']
     chromosomes = config.refdir_data(args['refdir'])['params']['chromosomes']
 
     workflow = pypeliner.workflow.Workflow()
 
-    workflow.setobj(
-        obj=mgd.OutputChunks('sample_id'),
-        value=samples)
-
     if run_remixt:
         workflow.subworkflow(
             name='remixt',
             func=remixt.create_remixt_workflow,
-            axes=('sample_id',),
             args=(
-                mgd.InputFile("tumour.bam", 'sample_id', fnames=tumours,
-                              extensions=['.bai']),
-                mgd.InputFile("normal.bam", 'sample_id', fnames=normals,
-                              extensions=['.bai']),
-                mgd.InputFile("breakpoints", 'sample_id', fnames=breakpoints),
-                mgd.InputInstance('sample_id'),
-                mgd.OutputFile('remixt.h5', 'sample_id', template=remixt_outfile),
-                mgd.OutputFile('remixt_brk_cn.csv', 'sample_id', template=remixt_brk_cn_csv),
-                mgd.OutputFile('remixt_cn.csv', 'sample_id', template=remixt_cn_csv),
-                mgd.OutputFile('remixt_minor_modes.csv', 'sample_id', template=remixt_minor_modes_csv),
-                mgd.OutputFile('remixt_mix.csv', 'sample_id', template=remixt_mix_csv),
-                mgd.OutputFile('remixt_read_depth.csv', 'sample_id', template=remixt_read_depth_csv),
-                mgd.OutputFile('remixt_stats.csv', 'sample_id', template=remixt_stats_csv),
+                mgd.InputFile(tumour, extensions=['.bai']),
+                mgd.InputFile(normal, extensions=['.bai']),
+                mgd.InputFile(breakpoints),
+                sample_id,
+                mgd.OutputFile(remixt_outfile),
+                mgd.OutputFile(remixt_brk_cn_csv),
+                mgd.OutputFile(remixt_cn_csv),
+                mgd.OutputFile(remixt_minor_modes_csv),
+                mgd.OutputFile(remixt_mix_csv),
+                mgd.OutputFile(remixt_read_depth_csv),
+                mgd.OutputFile(remixt_stats_csv),
                 refdir_paths['refdata_remixt'],
-                mgd.Template('rawdir', 'sample_id', template=remixt_raw_dir),
+                remixt_raw_dir,
                 refdir_paths['reference'],
                 chromosomes
             ),
@@ -99,20 +87,18 @@ def copynumber_calling_workflow(args):
             func=titan.create_titan_workflow,
             axes=('sample_id',),
             args=(
-                mgd.InputFile("tumour.bam", 'sample_id', fnames=tumours,
-                              extensions=['.bai']),
-                mgd.InputFile("normal.bam", 'sample_id', fnames=normals,
-                              extensions=['.bai']),
-                mgd.InputFile("target_list", 'sample_id', fnames=targets),
-                mgd.OutputFile('outfile', 'sample_id', template=titan_outfile),
-                mgd.OutputFile('params', 'sample_id', template=titan_params),
-                mgd.OutputFile('segs', 'sample_id', template=titan_segs),
-                mgd.OutputFile('igv_segs', 'sample_id', template=titan_igv_segs),
-                mgd.OutputFile('parsed', 'sample_id', template=titan_parsed),
-                mgd.OutputFile('plots', 'sample_id', template=titan_plots),
-                mgd.OutputFile('tar_outputs', 'sample_id', template=titan_tar_outputs),
-                mgd.OutputFile('museq.vcf', 'sample_id', template=museq_vcf),
-                mgd.InputInstance('sample_id'),
+                mgd.InputFile(tumour,extensions=['.bai']),
+                mgd.InputFile(normal,extensions=['.bai']),
+                mgd.InputFile(targets),
+                mgd.OutputFile(titan_outfile),
+                mgd.OutputFile(titan_params),
+                mgd.OutputFile(titan_segs),
+                mgd.OutputFile(titan_igv_segs),
+                mgd.OutputFile(titan_parsed),
+                mgd.OutputFile(titan_plots),
+                mgd.OutputFile(titan_tar_outputs),
+                mgd.OutputFile(museq_vcf),
+                sample_id,
                 refdir_paths['reference'],
                 chromosomes,
                 refdir_paths['het_positions_titan'],
@@ -148,15 +134,13 @@ def copynumber_calling_workflow(args):
             museq_vcf,
         ]
 
-    outputted_filenames = helpers.expand_list(filenames, samples, "sample_id")
-
     workflow.transform(
         name='generate_meta_files_results',
         func='wgs.utils.helpers.generate_and_upload_metadata',
         args=(
             sys.argv[0:],
             args["out_dir"],
-            outputted_filenames,
+            filenames,
             mgd.OutputFile(meta_yaml)
         ),
         kwargs={
