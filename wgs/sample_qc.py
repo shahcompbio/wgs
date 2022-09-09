71049d59debf4bb0429f2ebf6bb559aeba33430f
Diljot Grewal <diljot.grewal@gmail.com>
2022-09-09 11:49:46 -0700

    v0.2.9

--- wgs/sample_qc.py
+++ wgs/sample_qc.py
@@ -7,100 +7,71 @@ from wgs.config import config
 from wgs.utils import helpers
 from wgs.workflows import sample_qc
 
-def make_inputs(inputs, normal_only=False):
-    samples = list(inputs.keys())
-    roh = {sample: inputs[sample]['roh'] for sample in samples}
-    germline = {sample: inputs[sample]['germline_calls'] for sample in samples}
-    normal = {sample: inputs[sample]['normal_bam'] for sample in samples}
-
-    tumor=None
-    titan=None
-    remixt=None
-    breakpoints=None
-    somatic=None
-
-    if not normal_only:
-        tumor = {sample: inputs[sample]['tumour_bam'] for sample in samples}
-        titan = {sample: inputs[sample]['titan'] for sample in samples}
-        remixt = {sample: inputs[sample]['remixt'] for sample in samples}
-        breakpoints = {sample: inputs[sample]['breakpoints_consensus'] for sample in samples}
-        somatic = {sample: inputs[sample]['somatic_calls'] for sample in samples}
-
-    return {"normal":normal, "tumor": tumor, "roh":roh, "germline":germline, 
-        "titan": titan, "remixt": remixt, "breakpoints": breakpoints, "somatic": somatic}
-
 
 def sample_qc_workflow(args):
     inputs = helpers.load_yaml(args['input_yaml'])
-    normal_only = args['normal_only']
-    samples = list(inputs.keys())
 
-    # inputs
+    roh = inputs['roh']
+    germline = inputs['germline_calls']
+    normal = inputs['normal_bam']
+    tumour = inputs.get('tumour_bam', None)
+    titan = inputs.get('titan', None)
+    remixt = inputs.get('remixt', None)
+    breakpoints = inputs.get('breakpoints_consensus', None)
+    somatic = inputs.get('somatic_calls', None)
+    sample_id = inputs['sample_id']
+
     chromosomes = config.refdir_data(args['refdir'])['params']['chromosomes']
-    files = make_inputs(inputs, normal_only=normal_only)
 
     # outputs
-    out_dir = args['out_dir']
-    normal_coverage = os.path.join(out_dir, '{sample_id}', '{sample_id}_normal_coverage.tsv')
-    genome_wide_plot = os.path.join(out_dir, '{sample_id}', '{sample_id}_genome_wide.pdf')
+    normal_coverage = args['output_prefix'] + '_normal_coverage.tsv'
+    genome_wide_plot = args['output_prefix'] + '_genome_wide.pdf'
 
-    if not normal_only:
-        circos_plot_remixt = os.path.join(out_dir, '{sample_id}', '{sample_id}_circos_remixt.pdf')
-        circos_plot_titan = os.path.join(out_dir, '{sample_id}', '{sample_id}_circos_titan.pdf')
-        tumour_coverage = os.path.join(out_dir, '{sample_id}', '{sample_id}_tumour_coverage.tsv')
-
+    circos_plot_remixt = args['output_prefix'] + '_circos_remixt.pdf'
+    circos_plot_titan = args['output_prefix'] + '_circos_titan.pdf'
+    tumour_coverage = args['output_prefix'] + '_tumour_coverage.tsv'
 
     pyp = pypeliner.app.Pypeline(config=args)
     workflow = pypeliner.workflow.Workflow()
 
-    workflow.setobj(
-        obj=mgd.OutputChunks('sample_id'),
-        value=samples
-    )
-
-    if normal_only:
+    if args['normal_only']:
         workflow.subworkflow(
             name="normal_sample_qc",
             func=sample_qc.create_sample_qc_workflow_normal_only,
             ctx=helpers.get_default_ctx(),
-            axes=('sample_id',),
             args=(
-                mgd.InputInstance('sample_id'),
+                sample_id,
                 args["refdir"],
-                mgd.InputFile('normal.bam', 'sample_id', fnames=files["normal"]),
-                mgd.InputFile('roh', 'sample_id', fnames=files["roh"]),
-                mgd.InputFile('germline_calls', 'sample_id', fnames=files["germline"]),
-                mgd.OutputFile('genome_wide_plot.pdf', 'sample_id', template=genome_wide_plot),
-                mgd.OutputFile('normcov', 'sample_id', template=normal_coverage),
+                mgd.InputFile(normal),
+                mgd.InputFile(roh),
+                mgd.InputFile(germline),
+                mgd.OutputFile(genome_wide_plot),
+                mgd.OutputFile(normal_coverage),
                 chromosomes,
                 args['bins'],
                 args['mapping_qual_threshold']
             ),
-            # kwargs={'single_node': args['single_node']}
         )
-        outputted_filenames = helpers.expand_list(
-            [normal_coverage,genome_wide_plot],
-            samples, "sample_id")
+        outputted_filenames = [normal_coverage, genome_wide_plot]
     else:
         workflow.subworkflow(
             name="sample_qc",
             func=sample_qc.create_sample_qc_workflow,
             ctx=helpers.get_default_ctx(),
-            axes=('sample_id',),
             args=(
-                mgd.InputInstance('sample_id'),
+                sample_id,
                 args["refdir"],
-                mgd.InputFile('normal.bam', 'sample_id', fnames=files["normal"]),
-                mgd.InputFile('tumour.bam', 'sample_id', fnames=files["tumor"]),
-                mgd.InputFile('titan', 'sample_id', fnames=files["titan"]),
-                mgd.InputFile('remixt', 'sample_id', fnames=files["remixt"]),
-                mgd.InputFile('breakpoints_consensus', 'sample_id', fnames=files["breakpoints"]),
-                mgd.InputFile('roh', 'sample_id', fnames=files["roh"]),
-                mgd.InputFile('germline_calls', 'sample_id', fnames=files["germline"]),
-                mgd.InputFile('somatic_calls', 'sample_id', fnames=files["somatic"]),
-                mgd.OutputFile('genome_wide_plot.pdf', 'sample_id', template=genome_wide_plot),
-                mgd.OutputFile('normcov', 'sample_id', template=normal_coverage),
-                mgd.OutputFile('tumcov', 'sample_id', template=tumour_coverage),
+                mgd.InputFile(normal),
+                mgd.InputFile(tumour),
+                mgd.InputFile(titan),
+                mgd.InputFile(remixt),
+                mgd.InputFile(breakpoints),
+                mgd.InputFile(roh),
+                mgd.InputFile(germline),
+                mgd.InputFile(somatic),
+                mgd.OutputFile(genome_wide_plot),
+                mgd.OutputFile(normal_coverage),
+                mgd.OutputFile(tumour_coverage),
                 chromosomes,
                 args['bins'],
                 args['mapping_qual_threshold']
@@ -114,28 +85,23 @@ def sample_qc_workflow(args):
                 walltime='24:00',
                 disk=400
             ),
-            axes=('sample_id',),
             func=sample_qc.circos_plot,
             args=(
-                mgd.InputFile('titan', 'sample_id', fnames=files["titan"]),
-                mgd.InputFile('remixt', 'sample_id', fnames=files["remixt"]),
-                mgd.InputInstance("sample_id"),
-                mgd.InputFile(
-                    'breakpoints_consensus', 'sample_id', 
-                    fnames=files["breakpoints"]),
-                mgd.OutputFile(
-                    'circos_remixt', 'sample_id', 
-                    template=circos_plot_remixt),
-                mgd.OutputFile('circos_titan', 'sample_id', template=circos_plot_titan),
+                mgd.InputFile(titan),
+                mgd.InputFile(remixt),
+                sample_id,
+                mgd.InputFile(breakpoints),
+                mgd.OutputFile(circos_plot_remixt),
+                mgd.OutputFile(circos_plot_titan),
             ),
         )
-        outputted_filenames = helpers.expand_list(
-            [circos_plot_remixt, circos_plot_titan, 
-            normal_coverage, tumour_coverage,genome_wide_plot],
-            samples, "sample_id")
+        outputted_filenames = [
+            circos_plot_remixt, circos_plot_titan,
+            normal_coverage, tumour_coverage, genome_wide_plot
+        ]
 
-    meta_yaml = os.path.join(out_dir, 'metadata.yaml')
-    input_yaml_blob = os.path.join(out_dir, 'input.yaml')
+    meta_yaml = os.path.join(args['out_dir'], 'metadata.yaml')
+    input_yaml_blob = os.path.join(args['out_dir'], 'input.yaml')
 
     workflow.transform(
         name='generate_meta_files_results',
